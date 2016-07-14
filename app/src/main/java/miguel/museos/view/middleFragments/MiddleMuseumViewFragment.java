ecc24ceaabee06a2c9711a170bb525a6317b26a5
Miguel <oscarmiguel15@hotmail.com>
2016-07-14 00:28:19 -0500

    o

--- app/src/main/java/miguel/museos/view/middleFragments/MiddleMuseumViewFragment.java
+++ app/src/main/java/miguel/museos/view/middleFragments/MiddleMuseumViewFragment.java
@@ -1,15 +1,15 @@
 package miguel.museos.view.middleFragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import miguel.museos.R;
+import miguel.museos.data.Museum;
 
-i
+
 /**
  * Created by miguel on 14/07/16.
  */
@@ -17,13 +17,20 @@ public class MiddleMuseumViewFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
 
         View view =inflater.inflate(R.layout.fragment_museum_view, container , false);
+
 
 
         return view;
 
     }
+
+
+
+    private Museum museum;
+
+
 }
